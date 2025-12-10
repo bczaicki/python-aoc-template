34f87a1deddc2a952630cc8f054f768bcfd2d41b
Brian Czaicki <brian@greenthumb.market>
2025-12-09 17:54:17 -0800

    Day 4 part 2

--- day4/solution/day04.py
+++ day4/solution/day04.py
@@ -47,10 +47,50 @@ class Solution(BaseSolution):
 
 
 
+    def find_accessible_rolls(self, grid):
+        """Find all rolls that can be accessed (have < 4 @ neighbors)."""
+        accessible = []
+        ROLL_VALUE = '@'
+        row_len = len(grid)
+        col_len = len(grid[0])
+
+        for r_index, row in enumerate(grid):
+            for c_index, col in enumerate(row):
+                if grid[r_index][c_index] != ROLL_VALUE:
+                    continue
+                neighborsList = neighbors_8(r_index, c_index)
+                neighbors = self.filter_invalid_neighbors(
+                    neighborsList,
+                    row_len,
+                    col_len
+                )
+                rolls = list(filter(lambda neighbor: grid[neighbor[0]][neighbor[1]] == ROLL_VALUE, neighbors))
+                if len(rolls) < 4:
+                    accessible.append((r_index, c_index))
+        return accessible
+
     def part2(self):
         """Solve part 2."""
-        # TODO: Implement part 2
-        pass
+        # Create a mutable copy of the grid
+        grid = [list(row) for row in self.data]
+        total_removed = 0
+
+        while True:
+            # Find all accessible rolls
+            accessible = self.find_accessible_rolls(grid)
+
+            # If no more accessible rolls, we're done
+            if not accessible:
+                break
+
+            # Remove all accessible rolls
+            for r, c in accessible:
+                grid[r][c] = '.'
+
+            # Add to total
+            total_removed += len(accessible)
+
+        return total_removed
 
 
 if __name__ == "__main__":
